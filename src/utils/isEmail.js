84458f3382f96a0ff49f17a342589e20b1aced56
torjuman <torjumanrimon7@gmail.com>
2022-12-06 22:40:41 +0600

    Utils - email checking function

---
+++ src/utils/isEmail.js
@@ -0,0 +1,10 @@
+const isEmail = function (email) {
+  return String(email)
+    .replace(/\s+/g, "")
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/
+    );
+};
+
+module.exports = isEmail;
