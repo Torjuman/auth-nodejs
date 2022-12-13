503106c221be28a1206a6e29f1c8754cb6bc2b44
torjuman <torjumanrimon7@gmail.com>
2022-12-13 12:12:46 +0600

    controller - bug fix in changePassword

--- src/controllers/changePassword.controller.js
+++ src/controllers/changePassword.controller.js
@@ -9,7 +9,7 @@ const cookieOptions = require("../utils/cookieOptions");
  * @route http://localhost:80/api/v1/auth/password/change
  * @description User will submit oldPassword and newPassword then we will change the old password
  * @parameters oldPassword, newPassword, confirmNewPassword
- * @returns success message - email send
+ * @returns success message - user object
  ******************************************************/
 const changePassword = asyncHandler(async (res, req, next) => {
   let { email } = req.user;
@@ -28,19 +28,25 @@ const changePassword = asyncHandler(async (res, req, next) => {
     );
   }
 
-  const passwordUser = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-  passwordUser.password = newPassword;
+  const passwordValidation = user.comparePassword(oldPassword);
 
-  await passwordUser.save();
+  if (!passwordValidation) {
+    throw new CustomError("Invalid credentials", 400);
+  }
 
-  const token = passwordUser.getJwtToken();
-  passwordUser.password = undefined;
+  user.password = newPassword;
+
+  await user.save();
+
+  const token = user.getJwtToken();
+  user.password = undefined;
 
   res.cookie("token", token, cookieOptions);
   res.status(200).json({
     success: true,
-    passwordUser,
+    user,
   });
 });
 
